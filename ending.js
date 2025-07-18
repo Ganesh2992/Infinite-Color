371b5935c6a2f7874c5bd59a10f26977eecfa900
Ganesh2992 <ganeshmangalamsinha@gmail.com>
2025-07-19 00:33:43 +0530

    initial

---
+++ ending.js
@@ -0,0 +1,33 @@
+    // Generate a Random Color //
+
+const randomColor = function(){
+        const hex = "0123456789ABCDEF";
+        let color = '#'
+        for(let i = 0;i<6;i++){
+            color+= hex[Math.floor(Math.random() * 16)];
+        }
+        return color;
+};
+let intervalId=null; 
+
+ 
+const stopChangingColor = 
+
+document.querySelector('#start').addEventListener('click',function(){
+    if(intervalId === null){
+        intervalId = setInterval(changeColor,1000);
+        function changeColor(){
+        document.body.style.backgroundColor = randomColor();
+        }
+        console.log("Started");
+    }
+});
+
+document.querySelector('#stop').addEventListener('click',function(){
+    clearInterval(intervalId);
+    intervalId = null;
+    console.log("Stopped");
+    
+});
+
+   
